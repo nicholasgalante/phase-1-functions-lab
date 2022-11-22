380befb05c6dad52be6f0c73c420875b4ed9915b
Nicholas <nicholasgalante94@gmail.com>
2022-11-21 21:15:47 -0500

    commit tree adfd69ce64e8b264c5eab89b36a21d0e442f9d4d

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+distanceFromHqInBlocks = (location) => {
+    return Math.abs(location - 42);
+}
+
+
+distanceFromHqInFeet = (location) => {
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+distanceTravelledInFeet = (start, destination) => {
+    return Math.abs((destination - start) * 264);
+}
+
+calculatesFarePrice = (start, destination) => {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance < 400){
+        return 0;
+    } else if (distance < 2000 && distance > 400) {
+        return (distance - 400) * 0.02;
+    }
+    else if (distance > 2000 && distance < 2500) {
+        return 25;
+    }   
+    else {
+        return 'cannot travel that far';
+    }
+}
